27d649c861b416995deff4a296a96ea891f32f99
Damian Kokot <damkokot@gmail.com>
2022-02-14 12:06:42 +0100

    setting proper paths accroding to the project structure

--- src/sh/make_dataset.sh
+++ src/sh/make_dataset.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-fasta=data/hg19.ml.fa
-blacklist=data/wgEncodeHg19ConsensusSignalArtifactRegions.bed
+fasta=data/basenji_peak/hg19.ml.fa
+blacklist=data/basenji_peak/wgEncodeHg19ConsensusSignalArtifactRegions.bed
 
 if [ ! -e $fasta ]
 then
@@ -13,7 +13,7 @@ if [ ! -e $blacklist ]
 then
 	curl -O https://personal.broadinstitute.org/anshul/projects/encode/rawdata/blacklists/wgEncodeHg19ConsensusSignalArtifactRegions.bed.gz
 	gunzip wgEncodeHg19ConsensusSignalArtifactRegions.bed.gz
-	mv wgEncodeHg19ConsensusSignalArtifactRegions.bed data/
+	mv wgEncodeHg19ConsensusSignalArtifactRegions.bed data/basenji_data
 fi
 
-basenji_data.py -b $blacklist --local -p 8 -r 4096 -w 192 -l 1344 --peaks -v .12 -t .12 --stride 192 --stride_test 192 --crop 576 -o data_basset $fasta data/targets.txt
+../../basenji/bin/basenji_data.py -b $blacklist --local -p 8 -r 4096 -w 192 -l 1344 --peaks -v .12 -t .12 --stride 192 --stride_test 192 --crop 576 -o ../../data/basenji_peak $fasta ../../config/targets.txt
