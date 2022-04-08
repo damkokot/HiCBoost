f208489da75cab76ded077c7ddbac92b8815bfaf
Damian Kokot <damkokot@gmail.com>
2022-04-08 19:28:23 +0200

    setting contact sequences range, condition that  handles negative values of start indexes of input sequences

--- src/py/extract_hic.py
+++ src/py/extract_hic.py
@@ -4,17 +4,6 @@ import numpy as np
 import hicstraw
 
 
-# start = 74443629
-# end = 74443821
-# m = int(((start+end)/2)/5000)*5000
-# start = m - 1000000
-# end = m + 1000000
-# mat_obj_chr5 = hic.getMatrixZoomData('5','5', 'observed', 'KR', 'BP', 5000 )
-# numpy_mat_obj_chr5 = mat_obj_chr5.getRecordsAsMatrix(start, end, m , m + 4999 )
-# print(len(numpy_mat_obj_chr5))
-
-
-
 def load_test(bedfile):
 	"""
 	Loading bed format file with test sequences.
@@ -38,6 +27,7 @@ def hic_sequences(test_sequences, hic_data, res, rng):
 	
 	for record in test_sequences:
 		chr_num, start_seq, end_seq = record
+		seq_len = int(end_seq) - int(start_seq)
 		# print(type(chr_num[3:]), start_seq, end_seq, res)
 		contact_obj = hic_data.getMatrixZoomData(chr_num[3:], chr_num[3:], 'observed', 'KR', 'BP', res)
 
@@ -45,31 +35,42 @@ def hic_sequences(test_sequences, hic_data, res, rng):
 		margin = int(((int(start_seq)+int(end_seq))/2)/res)*res
 		
 		# create vector with contacts
-		contact_vector = contact_obj.getRecordsAsMatrix(margin-rng, margin+rng, margin, margin+res-1)
-		yield contact_vector
-	# 	contact_vectors.append(contact_vector)
-	# print(len(contact_vectors))
+		# if range is too big, start index of matrix set to 0
+		# and end index to multiplicity of range value
+		if (margin - rng) <= 0:	
+			contact_vector = contact_obj.getRecordsAsMatrix(0, 2*rng-1, margin, margin+res-1)
+		else:
+			contact_vector = contact_obj.getRecordsAsMatrix(margin-rng, margin+rng-1, margin, margin+res-1)
+		
+		# check shape of contact vector
+		# should be (n,2) where n is range divided by resolution
+		if contact_vector.shape == (400, 1):
+
+		# 	# print(chr_num[3:], contact_vector)
+		# 	print(chr_num, margin-rng, margin+rng-1, margin, margin+res, contact_vector.shape)
+		# get best scores of bins
+		# given that set range of contact sequences
+		# NOT SURE IF IT IS IMPLEMENTED RIGHT!!!
+			best_contacts = np.argpartition(contact_vector, 20, axis=None)[-20:]
+			for contact_bin in best_contacts:
+				contact_margin = int((contact_bin*res)-(res/2))
+				start_ct, end_ct = int(contact_margin-seq_len/2), int(contact_margin+seq_len/2)
+				print(chr_num, start_ct, end_ct, seq_len)
+			
+
 	
-
-
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Extracting best neighbours of target sequences from HiC data')
 	parser.add_argument('hic', type=str, help='HiC format file')
 	parser.add_argument('--targets', type=str, help='bed format file with test targets')
 	parser.add_argument('--resolution', type=int, help='resolution for contact matrix')
-	parser.add_argument('--range', type=int)
+	parser.add_argument('--range', type=int, 
+		help='range, to and from, the center of target sequence; value should be multiplicity of resolution')
 	args = parser.parse_args()
 
 	hic = hicstraw.HiCFile(args.hic)
 	test_seqs = load_test(args.targets)
-	a = hic_sequences(test_seqs, hic, args.resolution, args.range)
-	
-
-
-	
-
-
-
-### TODO
-### parse sequences test, put each sequence to getMatrixZoomData object, create numpy end-startx1
-### take 20 highest scores, calculate their distance (200 bin )
+	hic_sequences(test_seqs, hic, args.resolution, args.range)
+	# contact_obj = hic.getMatrixZoomData('11', '11', 'observed', 'KR', 'BP', 5000)
+	# contact_vector = contact_obj.getRecordsAsMatrix(0, 2000000-1, 130000, 2000000-1)
+	# print(contact_vector)
