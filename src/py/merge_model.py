55fea269b5319f7e11ee52cfe215c979515bdf42
Damian Kokot <dkokot@rudy.mimuw.edu.pl>
2022-07-22 16:34:34 +0200

    merging two model, one is original from Basenji project, second is for sequences from HiC data

---
+++ src/py/merge_model.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+
+# import models
+import custom_model
+import hic_model
+
+import argparse
+import json
+import os
+import shutil
+import sys
+import time
+
+import pandas as pd
+import numpy as np
+from natsort import natsorted
+import tensorflow as tf
+from tensorflow import keras
+
+
+def load_args(args=None):
+	""" Parse command-line arguments. """
+	parser = argparse.ArgumentParser(description='Create custom model for sequences extracted from HiC maps')
+	parser.add_argument('-pm','--model', type=str, help='h5 file with trained model')
+	parser.add_argument('-p', '--params', type=str, help='file with parameters')
+	# parser.add_argument('-o', '--out_dir', type=str, help='path in which model will be stored')
+	return parser.parse_args(args)
+
+
+def merge(target_model, hic_model):
+	"""
+	Merging target model with hic model.
+	Outputs from those will be passed to dense block
+	that contains two Dense layers. Final output is
+	probability that specific target sequence given
+	its neighbors is accesibile in specific cell type.
+	Cell type is then determined on the basis of hic sequences.
+	"""
+
+	# set list of outputs
+	outs = [target_model.output] + hic_model.output
+
+	# set common denseblock for target and hic model
+	dense_merge = tf.keras.layers.Dense(16,
+		name='dense_merge')
+
+	currents = []
+	for out in outs:
+		currents.append(dense_merge(out))
+
+	merged_model = tf.keras.Model(inputs=outs, outputs=currents)
+	print(merged_model.output)
+
+	# apply batch norm and dropout
+	batch_normalization_merge = tf.keras.layers.BatchNormalization(momentum=0.90, 
+		gamma_initializer=None, name='batch_normalization_merge')
+	batch_norms = []
+	for current in merged_model.output:
+		batch_norms.append(batch_normalization_merge(current))
+	
+	dropout_merge = tf.keras.layers.Dropout(rate=0.2, name='dropout_merge')
+	dropouts = []
+	for batch_norm in batch_norms:
+		dropouts.append(dropout_merge(batch_norm))
+
+	# set final dense
+	dense_final_merge = tf.keras.layers.Dense(1,
+		name='dense_finale_merge',
+		activation='sigmoid')
+	dense_finals = []
+	for drop in dropouts:
+		dense_finals.append(dense_final_merge(drop))
+
+	# set final model, plot, and display summary
+	model = tf.keras.Model([target_model.input, hic_model.input], dense_finals)
+	# tf.keras.utils.plot_model(model, to_file='final_merged_model_hic.png', show_shapes=True)
+
+	# return model
+
+
+def main(args=None):
+	args = load_args(args)
+
+	# load pretrained model
+	pre_model = custom_model.load_trained_md(args.model)
+
+	# load parameters
+	params_model_target = custom_model.get_dense(args.params)
+
+	# set models
+	for_target_model = custom_model.build_dense(params_model_target, pre_model)
+
+	for_hic_model = hic_model.conv_block(pre_model)
+
+	# call model
+	merge(for_target_model, for_hic_model)
+
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
